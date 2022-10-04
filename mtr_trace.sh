6e08f1a8ddf4bbb43b907808917ce6c8ca417359
主菜单 <47970938+zhucaidan@users.noreply.github.com>
2022-10-04 03:37:38 -0700

    add new AS9929 IP group

--- mtr_trace.sh
+++ mtr_trace.sh
@@ -46,16 +46,26 @@ for i in {0..8}; do
 					else
 					echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;33m联通香港\033[0m"
 					fi
-				else				
-					grep -q "223\.120\."  /root/traceroute_testlog
+				else	
+					grep -q "162\.219\."  /root/traceroute_testlog
 					if [ $? == 0 ];then
-					echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;35m移动CMI\033[0m"
-					else
-						grep -q "221\.183\."  /root/traceroute_testlog
+						grep -q "218\.105\." /root/traceroute_testlog
 						if [ $? == 0 ];then
-						echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;35m移动cmi\033[0m"
+						echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;33m联通9929\033[0m"
 						else
-						echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:其他"
+						echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;33m联通10099\033[0m"
+						fi
+					else				
+						grep -q "223\.120\."  /root/traceroute_testlog
+						if [ $? == 0 ];then
+						echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;35m移动CMI\033[0m"
+						else
+							grep -q "221\.183\."  /root/traceroute_testlog
+							if [ $? == 0 ];then
+							echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:\033[1;35m移动cmi\033[0m"
+							else
+							echo -e "目标:${iplocal[i]}[${iplise[i]}]\t回程线路:其他"
+							fi
 						fi
 					fi
 				fi
